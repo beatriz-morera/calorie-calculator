05990f46836faec7c84ca873e4079a45cc393d19
Beatriz Morera <beatrizmorera2016@gmail.com>
2020-01-23 22:58:53 +0100

    Updates

---
+++ precache-manifest.8e3c0eb75505f6670be2e91a9eacd42d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b91d4692d00c221d4fe0db3031e30d0",
+    "url": "/calorie-calculator/index.html"
+  },
+  {
+    "revision": "e13cd8d9f0cfcbc5bb2c",
+    "url": "/calorie-calculator/static/css/2.ff8bd605.chunk.css"
+  },
+  {
+    "revision": "49cf51f993d97bd2b354",
+    "url": "/calorie-calculator/static/css/main.65ac5b22.chunk.css"
+  },
+  {
+    "revision": "e13cd8d9f0cfcbc5bb2c",
+    "url": "/calorie-calculator/static/js/2.c8ca845c.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/calorie-calculator/static/js/2.c8ca845c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "49cf51f993d97bd2b354",
+    "url": "/calorie-calculator/static/js/main.8109a312.chunk.js"
+  },
+  {
+    "revision": "bd58c196f0f54e4d7b13",
+    "url": "/calorie-calculator/static/js/runtime-main.e3da879e.js"
+  }
+]);
